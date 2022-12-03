23a53343baf2a746720b965259377882970b9fea
Peng Mong <pengmnt@gmail.com>
2022-12-03 16:19:39 -0600

    http requests for messages

---
+++ src/main/java/net/yorksolutions/pengmongthaoblogcmscapstonebe/controllers/MessageController.java
@@ -0,0 +1,11 @@
+package net.yorksolutions.pengmongthaoblogcmscapstonebe.controllers;
+
+import net.yorksolutions.pengmongthaoblogcmscapstonebe.entities.Message;
+import net.yorksolutions.pengmongthaoblogcmscapstonebe.services.MessageService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/message")
+@CrossOrigin
+public class MessageController {
+}
